2d47f75c7b70158b8c1a21346fe930706833d159
Petar Peychev <petar.peychev@theverygroup.com>
2023-09-22 12:17:26 +0100

    new deck of cards and printing it

---
+++ 02-cards/deck.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"♠", "♤", "♥", "♡", "♦", "♢", "♣", "♧"}
+	cardValues := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
---
+++ 02-cards/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	deck := newDeck()
+	deck.print()
+}
